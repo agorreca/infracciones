40040c0bab95e533471b66f8199cac12e71c1da4
Nicolas Agorreca <agorreca@gmail.com>
2016-05-10 23:39:51 -0300

    Removing node-mysql and adding Sequelize

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var requestify = require('requestify'); 
+var cheerio = require('cheerio');
+var q = require('q');
+var app = express();
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('mysql://root:root@localhost:3306/infracciones');
+
+app.get('/', function (req, res) {
+
+  res.send('Hello World')
+
+
+
+});
+
+app.listen(8081, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+exports = module.exports = app;
